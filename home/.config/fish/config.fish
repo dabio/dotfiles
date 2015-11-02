0b9def4fed2c8e0a27d6507774d203096c9adc14
Danilo <dabio@users.noreply.github.com>
2015-11-02 16:05:42 +0100

    Adding GOPATH

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -3,7 +3,7 @@ set PATH $HOME/.rbenv/bin $PATH
 set PATH $HOME/.rbenv/shims $PATH
 rbenv rehash > /dev/null ^&1
 
-set GOPATH ~/Sites/go
+set -x GOPATH $HOME/Sites/go
 
 # aliases
 alias uuidgen 'uuidgen | tr "[:upper:]" "[:lower:]"'
