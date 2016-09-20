7f5b0e74ff397bf03f93a491fe82904209cf4452
Danilo <d.braband@vaola.de>
2016-09-20 09:13:47 +0200

    direnv

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -3,6 +3,10 @@ status --is-interactive; and . (rbenv init -|psub)
 
 set -x GOPATH $HOME/Sites/go
 set -x HOMEBREW_CASK_OPTS --appdir=$HOME/Applications
+set -x EDITOR /usr/bin/vim
 
 # aliases
 alias uuidgen 'uuidgen | tr "[:upper:]" "[:lower:]"'
+
+# direnv
+eval (direnv hook fish)
