c5db9f7ded358ab7a90635fe6e1d83e3bc9ebb92
Danilo <dabio@users.noreply.github.com>
2016-11-11 11:04:29 +0100

    Signing git commits and homeshick integration

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -4,6 +4,13 @@ status --is-interactive; and . (rbenv init -|psub)
 set -x GOPATH $HOME/Sites/go
 set -x HOMEBREW_CASK_OPTS --appdir=$HOME/Applications
 set -x EDITOR /usr/bin/vim
+set -x HOMESHICK_DIR /usr/local/opt/homeshick
 
 # aliases
 alias uuidgen 'uuidgen | tr "[:upper:]" "[:lower:]"'
+
+# gnupg
+source $HOME/.config/fish/gnupg.fish
+
+# homeshick
+source /usr/local/opt/homeshick/homeshick.fish
---
+++ home/.config/fish/gnupg.fish
@@ -0,0 +1,10 @@
+if not begin
+    [ -f ~/.gnupg/agent-info ]
+    and kill -0 (cut -d: -f 2 ~/.gnupg/agent-info) ^/dev/null
+end
+    gpg-agent --daemon --no-grab --use-standard-socket --write-env-file ~/.gnupg/agent-info >/dev/null ^&1
+end
+
+set -g -x GPG_AGENT_INFO (cut -c 16- ~/.gnupg/agent-info)
+set -g -x GPG_TTY (tty)
+
