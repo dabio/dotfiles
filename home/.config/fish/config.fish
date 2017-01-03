3ce97cb6fc5488a91758eb8d71a76db722c0b7f4
Danilo <dabio@users.noreply.github.com>
2017-01-03 06:57:07 +0100

    vim-go improvements

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,6 +1,8 @@
 # To enable shims and autocompletion for rbenv.
 status --is-interactive; and . (rbenv init -|psub)
 
+# Postgres
+set -x PATH $PATH /Applications/Postgres.app/Contents/Versions/latest/bin
 set -x GOPATH $HOME/Sites/go
 set -x HOMEBREW_CASK_OPTS --appdir=$HOME/Applications
 set -x EDITOR /usr/bin/vim
