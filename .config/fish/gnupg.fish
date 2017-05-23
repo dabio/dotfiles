1a36981a359fcdb2b8b7066a62c5bd38ad5ec97b
Danilo <dabio@users.noreply.github.com>
2017-05-23 09:22:58 +0200

    gnupg v2

--- .config/fish/gnupg.fish
+++ .config/fish/gnupg.fish
@@ -2,7 +2,7 @@ if not begin
     [ -f ~/.gnupg/agent-info ]
     and kill -0 (cut -d: -f 2 ~/.gnupg/agent-info) ^/dev/null
 end
-    /usr/local/opt/gpg-agent/bin/gpg-agent --daemon --no-grab --use-standard-socket --write-env-file ~/.gnupg/agent-info >/dev/null ^&1
+    /usr/local/opt/gpg2/bin/gpg-agent --daemon --no-grab --use-standard-socket --write-env-file ~/.gnupg/agent-info >/dev/null ^&1
 end
 
 set -g -x GPG_AGENT_INFO (cut -c 16- ~/.gnupg/agent-info)
