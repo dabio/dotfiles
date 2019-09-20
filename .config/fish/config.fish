eaf6e155f2f3b231b297d3d0eb1426a2c6978490
Danilo <dabio@users.noreply.github.com>
2019-09-20 21:54:22 +0200

    Current settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,64 +1,21 @@
-source ~/.config/fish/aliases.fish
-
 if status --is-login
+    set -x EDITOR /usr/bin/nvim
+    set -x PYTHONDONTWRITEBYTECODE 1
+    set -x VOLTPATH $HOME/.vim/volt
 
-    set -x PATH $PATH $HOME/.local/bin
-
-    # Postgres
-    if test -d /Applications/Postgres.app/Contents/Versions/latest/bin
-        set -x PATH $PATH /Applications/Postgres.app/Contents/Versions/latest/bin
-    end
-
-    # go
-    set -x GOPATH $HOME/go
-    if test -d $GOPATH/bin
-        set -x PATH $PATH $GOPATH/bin
-    end
-
-    # ruby gems
-    if test -d $HOME/.gem/ruby/2.3.0/bin
-        set -x PATH $PATH $HOME/.gem/ruby/2.3.0/bin
-    end
-
-    # rust
-    if test -d $HOME/.cargo/bin
-        set -x PATH $PATH $HOME/.cargo/bin
-    end
-
-    # python3
-    #    if test -d $HOME/Library/Python/3.7/bin
-    #        set -x PATH $PATH $HOME/Library/Python/3.7/bin
-    #    end
-    # pyenv
-    if command --search pyenv > /dev/null do
-        source (pyenv init -|psub)
-    end
-    # poetry
     if test -d $HOME/.poetry/bin
         set -x PATH $PATH $HOME/.poetry/bin
-        source $HOME/.poetry/env
     end
 
-    if test -L /usr/local/bin/vim
-        set -x EDITOR /usr/local/bin/vim
-    else
-        set -x EDITOR /usr/bin/vim
-    end
-
-    # trash
-    if command --search trash > /dev/null do
-        alias rm "trash"
-    end
-
-    if status --is-interactive
-        eval sh $HOME/.dotfiles/.config/base16-shell/scripts/base16-default-dark.sh
-    end
-
-    # Don't write bytecode, Python!
-    set PYTHONDONTWRITEBYTECODE 1
-
-    # direnv
-    eval (direnv hook fish)
+    direnv hook fish | source
 end
 
-export GPG_TTY=(tty)
+alias pass gopass
+alias ip "dig +short myip.opendns.com @resolver1.opendns.com"
+alias cpu "sysctl -n machdep.cpu.brand_string"
+alias uuidgen 'uuidgen | tr "[:upper:]" "[:lower:]"'
+
+function battery
+    pmset -g batt | egrep "([0-9]+\%).*" -o --colour=auto | cut -f1 -d';'
+    pmset -g batt | egrep "([0-9]+\%).*" -o --colour=auto | cut -f3 -d';'
+end
